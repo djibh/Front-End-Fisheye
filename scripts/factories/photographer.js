83d805a88e652a08f6f2f178c002fd07847b4947
Djibh <julien.riera@protonmail.com>
2023-03-16 10:50:07 +0100

    style(home): add description & styling for articles

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,8 +9,21 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const description = document.createElement('div');
+        description.classList.add("description");
+        const location = document.createElement('h3')
+        location.textContent = `${city}, ${country}`
+        const tagLine = document.createElement('h4');
+        tagLine.textContent = tagline;
+        const priceTag = document.createElement('h5');
+        priceTag.textContent = `${price}€/jour`;
+        description.appendChild(location);
+        description.appendChild(tagLine);
+        description.appendChild(priceTag);
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(description);
         return (article);
     }
     return { name, picture, getUserCardDOM }
