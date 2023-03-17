e9cb31c9fa00a88194eea735eadb6343571ab6c3
Djibh <julien.riera@protonmail.com>
2023-03-17 09:52:03 +0100

    feat(home): add links and alt for logo and articles

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,30 +1,47 @@
 function photographerFactory(data) {
-    const { name, city, country, tagline, price, portrait } = data;
+  const { name, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        const description = document.createElement('div');
-        description.classList.add("description");
-        const location = document.createElement('h3')
-        location.textContent = `${city}, ${country}`
-        const tagLine = document.createElement('h4');
-        tagLine.textContent = tagline;
-        const priceTag = document.createElement('h5');
-        priceTag.textContent = `${price}€/jour`;
-        description.appendChild(location);
-        description.appendChild(tagLine);
-        description.appendChild(priceTag);
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const link = createArticleLink();
+    const description = createArticleDescription();
 
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(description);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+    article.appendChild(link);
+    article.appendChild(description);
+    return article;
+  }
+
+  function createArticleLink() {
+    const link = document.createElement("a");
+    link.setAttribute("href", "/");
+    link.setAttribute("alt", name);
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    link.appendChild(img);
+    link.appendChild(h2);
+
+    return link;
+  }
+
+  function createArticleDescription() {
+    const description = document.createElement("div");
+    description.classList.add("description");
+    const location = document.createElement("h3");
+    location.textContent = `${city}, ${country}`;
+    const tagLine = document.createElement("h4");
+    tagLine.textContent = tagline;
+    const priceTag = document.createElement("h5");
+    priceTag.textContent = `${price}€/jour`;
+    description.appendChild(location);
+    description.appendChild(tagLine);
+    description.appendChild(priceTag);
+
+    return description;
+  }
+
+  return { name, picture, getUserCardDOM };
 }
